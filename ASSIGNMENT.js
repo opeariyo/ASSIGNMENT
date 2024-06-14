5fbe315ad4175a4d3eec7b526b23e50b634a1246
opeariyo <opemiariyo@gmail.com>
2024-06-14 13:33:26 +0100

    assignment commit

---
+++ ASSIGNMENT.js
@@ -0,0 +1,10 @@
+const number = 25
+if (number % 2 === 0) {
+   console.log('even number')
+
+} else (
+   console.log('odd number')
+)
+
+
+
---
+++ ASSIGNMENT2.js
@@ -0,0 +1,18 @@
+const side1 = 9
+const side2 = 7
+const side3 = 8
+
+if(side1 == side2 == side3) {
+    console.log('EQUILATERAL TRIANGLE')
+
+} else if (side1 === side2 || side1 === side3 || side2 ===side3) (
+    console.log('ISOSCELES TRIANGLE')
+) 
+else {
+    console.log('SCALENE TRIANGLE')
+}
+
+
+
+
+
